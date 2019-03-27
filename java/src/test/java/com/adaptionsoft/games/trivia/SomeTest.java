9580cd0f715b85ae417dbd34bc404d3fcf4c0610
Gowtham Shanmugaraj Ganesan <gowthamraj07@gmail.com>
2019-03-27 15:33:41 +0100

    full coverage with more seeds

--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -2,12 +2,33 @@ package com.adaptionsoft.games.trivia;
 
 import static org.junit.Assert.*;
 
+import com.adaptionsoft.games.trivia.runner.GameRunner;
+import org.approvaltests.Approvals;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Random;
 
 public class SomeTest {
 
 	@Test
-	public void true_is_true() throws Exception {
+	@Ignore
+	public void true_is_true() {
 		assertTrue(false);
 	}
+
+	@Test
+	public void shouldProduceSameOutput() {
+		Integer[] seed = new Integer[]{0,1,2,3,4,5};
+		Approvals.verifyAll(seed,this::runGame);
+	}
+
+	private String runGame(Integer seed) {
+		ByteArrayOutputStream outputStream1 = new ByteArrayOutputStream();
+		GameRunner.runGame(new Random(seed), new PrintStream(outputStream1));
+		return outputStream1.toString();
+	}
+
 }
